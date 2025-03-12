db070deb1c84d5d009caa3c135d225586478a09b
Gem Seeker <gemseeker24@gmail.com>
2025-03-12 12:34:06 +0800

    added OrderDao

--- app/src/main/java/io/zak/delivery/data/AppDatabase.java
+++ app/src/main/java/io/zak/delivery/data/AppDatabase.java
@@ -3,9 +3,13 @@ package io.zak.delivery.data;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import io.zak.delivery.data.dao.ConsumerDao;
+import io.zak.delivery.data.dao.OrderDao;
 import io.zak.delivery.data.dao.ProductDao;
 import io.zak.delivery.data.dao.UserDao;
 import io.zak.delivery.data.dao.VehicleStockDao;
+import io.zak.delivery.data.entities.Consumer;
+import io.zak.delivery.data.entities.Order;
 import io.zak.delivery.data.entities.Product;
 import io.zak.delivery.data.entities.User;
 import io.zak.delivery.data.entities.VehicleStock;
@@ -13,10 +17,14 @@ import io.zak.delivery.data.entities.VehicleStock;
 @Database(entities = {
         User.class,
         Product.class,
-        VehicleStock.class
+        VehicleStock.class,
+        Consumer.class,
+        Order.class
 }, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract UserDao users();
     public abstract ProductDao products();
     public abstract VehicleStockDao vehicleStocks();
+    public abstract ConsumerDao consumers();
+    public abstract OrderDao orders();
 }
